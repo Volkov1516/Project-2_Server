0ca11f7cd41502101ad53e136151994fa6e8d146
Vlad Volkov <volkov.xvi@icloud.com>
2025-11-15 14:24:22 +0200

    feat: refactor

--- src/models/project.model.ts
+++ src/models/project.model.ts
@@ -9,12 +9,11 @@ type Project = {
 export const createProjectModel = async (
   data: Partial<Project>,
 ): Promise<Project> => {
-  console.log("DB_NAME =", process.env.DB_NAME);
-
   const result = await pool.query(
-    "INSERT INTO projects (ownerId, name) VALUES ($1, $2) RETURNING *",
+    "INSERT INTO projects (owner_id, name) VALUES ($1, $2) RETURNING *",
     [data.ownerId, data.name],
   );
+
   return result.rows[0];
 };
 
@@ -46,7 +45,7 @@ export const updateProjectModel = async (
   data: Partial<Project>,
 ): Promise<Project | undefined> => {
   const result = await pool.query(
-    "UPDATE projects SET ownerId = COALESCE($1, ownerId), name = COALESCE($2, name) WHERE id=$3 RETURNING *",
+    "UPDATE projects SET owner_id = COALESCE($1, owner_id), name = COALESCE($2, name) WHERE id=$3 RETURNING *",
     [data.ownerId, data.name, id],
   );
   return result.rows[0];
